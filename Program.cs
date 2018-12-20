d3a81ddcf2b899f22ee5a4726ebe06e6e0ba0ec5
Gaurav Chand <46017862+itsmegc@users.noreply.github.com>
2018-12-20 13:50:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Program
+    {
+        static void Change(ref int a)
+        {
+            a = a + 50;  
+            Console.WriteLine("The value inside change fn : " + a);
+        }
+        static void Main()
+        {
+            int a = 100;
+            Console.WriteLine("This is the value inside main fn " + a);
+            Change(ref a);
+            Console.WriteLine("This is the value inside main fn " + a); 
+           
+            Console.ReadKey();
+
+        }
+    }
+
